6f3cb198b831581a0b5568a22141aa151ada0a3a
lpg <1003289700@qq.com>
2017-09-01 18:21:37 +0800

    添加vue路由

--- resources/assets/dt/app.js
+++ resources/assets/dt/app.js
@@ -9,6 +9,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -17,6 +18,13 @@ window.Vue = require('vue');
 
 Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    //mode: 'history',
+    //routes,
 });
+
+new Vue({
+    router,
+}).$mount('#app');
